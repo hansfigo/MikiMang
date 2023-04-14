93a71db83228bec92b4c36bf1aba8ba9d92e46dc
hansfigo <hambasahaya0303@gmail.com>
2023-04-15 00:07:05 +0700

    new file:   .eslintignore 	new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   .npmrc 	new file:   .prettierignore 	new file:   .prettierrc 	new file:   .vscode/settings.json 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.cjs 	new file:   src/app.d.ts 	new file:   src/app.html 	new file:   src/app.postcss 	new file:   src/global.css 	new file:   src/index.test.ts 	new file:   src/lib/FetchData.svelte 	new file:   src/lib/MangaModal.svelte 	new file:   src/lib/mainPage.svelte 	new file:   src/routes/+layout.svelte 	new file:   src/routes/+page.svelte 	new file:   static/favicon.png 	new file:   svelte.config.js 	new file:   tailwind.config.cjs 	new file:   tsconfig.json 	new file:   vite.config.ts

---
+++ src/lib/MangaModal.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { modalStore } from '@skeletonlabs/skeleton';
+
+	export let parent: any;
+    export let card : string;
+
+	const cButton = 'fixed top-4 right-4 z-50 font-bold shadow-xl';
+	const cImage = 'max-w-[90%] max-h-[90%] rounded-container-token overflow-hidden shadow-xl';
+	const cBase = 'card p-4 w-[340px] shadow-xl space-y-4';
+	const cHeader = 'text-2xl font-bold';
+    console.log(card)
+
+</script>
+
+<!-- Button -->
+<div class="modal-example-form {cBase}">
+    <div class="">
+        <img class="{cImage} min-w-[50%]" src={$modalStore[0]?.image}/>
+    </div>
+	<header class={cHeader}>{$modalStore[0]?.title ?? '(title missing)'}</header>
+	<article class="max-h-24 overflow-auto">{$modalStore[0]?.body ?? '(body missing)'}</article>
+
+	<!-- prettier-ignore -->
+	<footer class="modal-footer">
+        <button class="btn variant-filled-primary" on:click={parent.onClose}>Read</button>
+        <button class="btn {parent.buttonNeutral}" on:click={parent.onClose}>{parent.buttonTextCancel}</button>
+    </footer>
+</div>
+<!-- <img src={$modalStore[0]?.image} class={cImage} alt="Example" title="Source: {$modalStore[0]?.meta.source}" /> -->
---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { AppBar, AppRail, AppRailTile, AppShell, LightSwitch } from '@skeletonlabs/skeleton';
+	import MainPage from '../lib/mainPage.svelte';
+	import { writable, type Writable } from 'svelte/store';
+
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar
+			shadow="shadow-lg"
+			gridColumns="grid-cols-3"
+			slotDefault="place-self-center"
+			slotTrail="place-content-end"
+		>
+			<svelte:fragment slot="lead">
+				<div class="container">
+					<h1>MikiMang</h1>
+				</div>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<button type="button" class="btn-icon variant-ghost-primary"
+				
+					></button
+				>
+				<button type="button" class="btn variant-ghost-primary">Github</button>
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<div class="h h-full p-4 w-52 bg-fuchsia-500">Halo Madoo</div>
+	</svelte:fragment>
+
+	<!-- Router Slot -->
+	<MainPage />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Created By FigoMager <br /> Powered By Svelte</svelte:fragment>
+</AppShell>
